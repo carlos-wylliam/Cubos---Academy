fc9a3051e60f2e42c1176d211621e7dbd83c8c75
CarlosWylliam <99105323+carlos-wylliam@users.noreply.github.com>
2023-06-21 18:45:04 -0300

    interando um vetor atraves de um loop

---
+++ modulo-1/Arrays-Loops/exercicio02.js
@@ -0,0 +1,6 @@
+//Faça um programa que imprima todos os elementos de um array que contém alguns números quaisquer.
+
+const numeros = [12, 13, 17, 7, 10]
+for(i = 0;i < numeros.length;i++){
+    console.log(numeros[i])
+}
