d84809825effba3956d11abc543535ed55f236f0
unknown <carloswylliam023@gmail.com>
2023-06-27 19:33:12 -0300

    arrays dentro de objetos e objetos dentro de array

---
+++ modulo-1/Objetos/objetos002.js
@@ -0,0 +1,23 @@
+let cartao = {
+    nome:"wylliam",
+    idade: 19,
+    produtosConsumidos:[
+        {
+            nomeProduto: "Batata",
+            precoUnit: 500,
+            qtdComprada: 20
+        },
+        {
+            nomeProduto: "Salmão",
+            precoUnit: 7000,
+            qtdComprada: 2
+        },
+        {
+            nomeProduto: "Coca-cola",
+            precoUnit: 800,
+            qtdComprada: 2
+        }
+    ]
+}
+
+console.log(cartao)
