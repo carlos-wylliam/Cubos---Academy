34ed1602527fcd7d43c9654b2301229d81b3947a
unknown <carloswylliam023@gmail.com>
2023-06-26 21:12:45 -0300

    abcedario

---
+++ modulo-1/hackerrank-arrays-loops/abcedario.js
@@ -0,0 +1,11 @@
+let qtdAcertos = 0
+let palavraErradas = 0
+
+for(let criancas of palavras){
+  if(criancas.substring(0,1) === letra){
+   
+  }else{
+    palavraErradas += 1
+  }
+}
+console.log(palavraErradas)
