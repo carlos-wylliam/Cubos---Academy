0fe2bf8e6a768a6b800ee00b5f9296604f254aac
unknown <carloswylliam023@gmail.com>
2023-06-29 20:51:55 -0300

    exercicio de padStart

---
+++ modulo-1/metodos-string/exercicio03.js
@@ -0,0 +1,12 @@
+function imprimirData (dia, mes, ano){
+    const diaFormatado = String(dia).padStart(2, "0")
+    const mesFormatado = String(mes).padStart(2, "0")
+    const anoFormatado = String(ano).padStart(2, "0")
+    console.log(`${diaFormatado}/${mesFormatado}/${anoFormatado}`)
+    
+
+}
+
+
+imprimirData(18,2,1991)
+imprimirData(1,1,2021)
