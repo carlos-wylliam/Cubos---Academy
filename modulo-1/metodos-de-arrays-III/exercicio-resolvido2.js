49e922e008303edbefadb121703ad24099ea0fa3
unknown <carloswylliam023@gmail.com>
2023-07-06 20:38:57 -0300

    exercicio resolvido 2

---
+++ modulo-1/metodos-de-arrays-III/exercicio-resolvido2.js
@@ -0,0 +1,36 @@
+const carros =  [
+    {
+        nome: 'corola',
+        marca: 'toyota',
+        ano: '2020',
+        cor: 'prata'
+    },
+    {
+        nome: 'argo',
+        marca: 'fiat',
+        ano: '2021',
+        cor: 'preto'
+    },
+    {
+        nome: 'ranger',
+        marca: 'ford',
+        ano: '2021',
+        cor: 'prata'
+    },
+    {
+        nome: 'hilux',
+        marca: 'toyota',
+        ano: '2018',
+        cor: 'branco'
+    }
+]
+
+const resultado = (arraysCarros, campo) =>{
+    const requesicao = arraysCarros.sort((a,b) => {
+        return a[campo].localeCompare(b[campo])
+    })
+
+    console.log(requesicao)
+}
+
+resultado(carros, 'ano')
