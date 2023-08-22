66a8945796f14efaa667e2048bbbbfa299fa6845
unknown <carloswylliam023@gmail.com>
2023-08-22 10:47:41 -0300

    aula conexao postgre

--- modulo-3/conexao-nodejs-postgresql-aula/index.js
+++ modulo-3/conexao-nodejs-postgresql-aula/index.js
@@ -40,10 +40,33 @@ const pool = require('./conexao')
 const app = express()
 
 app.get('/', async (req,res)=>{
+    // const { id } = req.params
+    const { pagina, porPagina } = req.query
     try {
-        const resultado = await pool.query('select * from empresas')
-        res.json(resultado)
+        // const query = 'select * from empresas where nome = $1 or nome = $2'
+        // const params = ['Google','Facebook']
 
+        // const query = 'update empresas set site = $1 where id = $2'
+        // const params = ['www.cakewalk.com',1]
+
+        // const query = 'select e.id as empresaId, f.id as filialId, e.nome, f.pais, p.nome as funcionario from empresas e join filiais f on e.id = f.empresa_id join pessoas p on e.id = p.empresa_id'
+
+        const query = 'select * from pessoas order by id asc limit $1 offset $2'
+
+        const { rowCount } = await pool.query('select * from pessoas')
+
+        const offset = pagina === 1?0 : (pagina -1) * porPagina
+        
+        const resultado = await pool.query(query,[porPagina,offset])
+
+        const result = {
+            pagina,
+            porPagina,
+            total: rowCount,
+            registros: resultado.rows,
+        }
+        
+        return res.json(result)
     } catch (error) {
         console.log(error.message)
     }
